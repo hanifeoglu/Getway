95188cb83b99eb9c7283047cd1c6ab18e26ce455
Uğur Arıcı <m.ugurarici@gmail.com>
2020-02-05 17:43:58 +0300

    Import muhammederdem / vue-interactive-paycard for payment page

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,38 +5,13 @@
  */
 
 require('./bootstrap');
-import card from './components/CardPaymentComponent.vue';
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import App from './paycard/src/App.vue'
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-
-Vue.component('card', require('./components/CardPaymentComponent.vue'));
+Vue.config.productionTip = false
 
 const app = new Vue({
-    el: '#app',
-    components: {
-        'card': require('./components/CardPaymentComponent.vue'),
-    }
-});
-
-
-
+    render: h => h(App)
+}).$mount('#app')
--- resources/views/paymentform.blade.php
+++ resources/views/paymentform.blade.php
@@ -3,9 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>CodePen - Credit Card Form - VueJs-last</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-    <link rel="stylesheet" href="{{asset('paymentform/style.css')}}">
-
 </head>
 <body>
 <!-- partial:index.partial.html -->
@@ -21,7 +18,7 @@
 </div>
 <!-- partial -->
 <script src='https://unpkg.com/vue-the-mask@0.11.1/dist/vue-the-mask.js'></script>
-<script  src="{{asset('paymentform/script.js')}}"></script>
+<script  src="{{mix('js/app.js')}}"></script>
 
 </body>
 
